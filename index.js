6d0bfb9c55ecf083366fa529372eb8a648c13857
AMO <abdullahomira25@gmail.com>
2026-05-08 15:19:07 +0300

    Revert "vercel"
    This reverts commit 1b073400e519cb72a58343c585f1d7bd19e61794.

--- index.js
+++ index.js
@@ -819,7 +819,7 @@ async function getSeriesStreams(rawId) {
   ];
 }
 
-async function handleRequest(req, res) {
+const server = http.createServer(async (req, res) => {
   try {
     const reqUrl = new URL(req.url || "/", `http://${req.headers.host || "localhost"}`);
     const pathname = reqUrl.pathname;
@@ -863,6 +863,8 @@ async function handleRequest(req, res) {
     console.error("[Grama]", error);
     return sendJson(res, 200, { streams: [] });
   }
-}
+});
 
-module.exports = { handleRequest };
+server.listen(PORT, HOST, () => {
+  console.log(`MiX addon listening on http://${HOST}:${PORT}/manifest.json`);
+});
